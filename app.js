e5b92bf8eb319e661dac416507ecbd6ad06756c9
Bijen Hirachan <bijen.hirachan@gmail.com>
2024-01-12 01:18:54 +0100

    Frontend url added

--- app.js
+++ app.js
@@ -7,27 +7,28 @@ const PORT = 3000;
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     methods: ["GET", "POST"],
+//     credentials: true,
+//   })
+// );
 
 const server = createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: "https://socketiochatter.netlify.app/",
     methods: ["GET", "POST"],
     credentials: true,
   },
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello world");
+// });
 
 io.on("connection", (socket) => {
   console.log(`User ${socket.id} connected`);
