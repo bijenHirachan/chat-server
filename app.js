6859f0b6fd80535f733700f85f7cc43f2d3f5b03
Bijen Hirachan <bijen.hirachan@gmail.com>
2024-01-12 01:59:02 +0100

    Slash removed from url

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const server = createServer(app);
 const io = new Server(server, {
   cors: {
     // origin: "http://localhost:5173",
-    origin: "https://socketiochatter.netlify.app/",
+    origin: "https://socketiochatter.netlify.app",
     methods: ["GET", "POST"],
     credentials: true,
   },
